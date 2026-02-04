98beea91681dd142de118075f73b377abb4d27de
Toufiq <toufiq.austcse@gmail.com>
2026-02-04 17:39:07 +0600

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // 1. Enable static export
     output: 'export',
-
-    // 2. Set the base path (required for GitHub Pages)
-    // Replace 'your-repo-name' with your actual repository name
-    basePath: process.env.NODE_ENV === 'production' ? '/tensai-study-aboard' : '',
-
-    // 3. Optional: Disable image optimization (GitHub Pages doesn't support the default Next.js Image Optimization)
+    basePath: '', // MUST BE EMPTY
     images: {
         unoptimized: true,
     },
 };
-
 module.exports = nextConfig;
