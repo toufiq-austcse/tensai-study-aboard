dbfc1bc9975c4f0933e37c6cdce81a6c64ef98ca
Toufiq <toufiq.austcse@gmail.com>
2026-02-04 17:25:28 +0600

    fix: image

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export const metadata: Metadata = {
     title: "Tensai Study Aboard",
     description: "Tensai Study Abroad helps you navigate the complex world of international education with personalized guidance and support at every step.",
     icons: {
-        icon: "/lovable-uploads/cd5a57fa-7ab9-4755-8744-d9764abded44.png",
-        apple: "/lovable-uploads/cd5a57fa-7ab9-4755-8744-d9764abded44.png",
+        icon: "./lovable-uploads/cd5a57fa-7ab9-4755-8744-d9764abded44.png",
+        apple: "./lovable-uploads/cd5a57fa-7ab9-4755-8744-d9764abded44.png",
     },
 };
 
